f978db7435f564b286f0d94829f0685a1bc2f47c
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-21 21:13:44 +0300

    hotfix

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -2,7 +2,7 @@ import sentencepiece as spm
 
 # train English
 spm.SentencePieceTrainer.Train(
-    input="data/WMT14_English_German/train.en",
+    input="../data/WMT14_English_German/train.en",
     model_prefix="en",
     vocab_size=50000,
     character_coverage=1.0,
@@ -11,7 +11,7 @@ spm.SentencePieceTrainer.Train(
 
 # train German
 spm.SentencePieceTrainer.Train(
-    input="data/WMT14_English_German/train.de",
+    input="../data/WMT14_English_German/train.de",
     model_prefix="de",
     vocab_size=50000,
     character_coverage=1.0,
