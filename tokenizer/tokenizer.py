7383342fb6609011e9e40a74f9c2452cb62dcbb2
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-21 10:44:51 +0300

    add tokenizer

---
+++ tokenizer/tokenizer.py
@@ -0,0 +1,19 @@
+import sentencepiece as spm
+
+# train English
+spm.SentencePieceTrainer.Train(
+    input="data/WMT14_English_German/train.en",
+    model_prefix="en",
+    vocab_size=50000,
+    character_coverage=1.0,
+    model_type="unigram",
+)
+
+# train German
+spm.SentencePieceTrainer.Train(
+    input="data/WMT14_English_German/train.de",
+    model_prefix="de",
+    vocab_size=50000,
+    character_coverage=1.0,
+    model_type="unigram",
+)
