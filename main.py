321e567a4443da9347d83a2d20c5b4beb8926262
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-22 00:00:35 +0300

    add set_global_seed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from dataset import WMTDataset
+from utils import set_global_seed
+
+set_global_seed(42)
 
 dataset = WMTDataset(
     from_lang_data_path="data/WMT14_English_German/train.en",
---
+++ utils.py
@@ -0,0 +1,17 @@
+import random
+
+import numpy as np
+import torch
+
+
+def set_global_seed(seed: int):
+    """
+    Set global seed for reproducibility.
+    """
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
