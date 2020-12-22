22a086e7b88bd585d6bf88724d8917871851e744
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-22 14:39:10 +0300

    add seq2seq model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from dataset import WMTCollator, WMTDataset
-from network import Seq2SeqRNNEncoder
+from network import Seq2SeqRNN
 from utils import set_global_seed
 
 # hyper-parameters
@@ -16,6 +16,11 @@ ENCODER_EMBEDDING_DIM = 300
 ENCODER_HIDDEN_SIZE = 300
 ENCODER_NUM_LAYERS = 1  # TODO: validate num_layers > 1
 ENCODER_DROPOUT = 0
+
+DECODER_EMBEDDING_DIM = 300
+DECODER_HIDDEN_SIZE = 300
+DECODER_NUM_LAYERS = 1  # TODO: validate num_layers > 1
+DECODER_DROPOUT = 0
 
 
 device = torch.device(DEVICE)
@@ -44,15 +49,20 @@ dataloader = DataLoader(
 )
 
 # model
-model = Seq2SeqRNNEncoder(
+model = Seq2SeqRNN(
     encoder_num_embeddings=dataset.from_lang_tokenizer.vocab_size(),
     encoder_embedding_dim=ENCODER_EMBEDDING_DIM,
     encoder_hidden_size=ENCODER_HIDDEN_SIZE,
     encoder_num_layers=ENCODER_NUM_LAYERS,
     encoder_dropout=ENCODER_DROPOUT,
+    decoder_num_embeddings=dataset.to_lang_tokenizer.vocab_size(),
+    decoder_embedding_dim=DECODER_EMBEDDING_DIM,
+    decoder_hidden_size=DECODER_HIDDEN_SIZE,
+    decoder_num_layers=DECODER_NUM_LAYERS,
+    decoder_dropout=DECODER_DROPOUT,
 ).to(device)
 
 # inference
 from_seq, to_seq = next(iter(dataloader))
 from_seq, to_seq = from_seq.to(device), to_seq.to(device)
-enc_last = model(from_seq)
+logits = model(from_seq, to_seq)
--- network.py
+++ network.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 
 
 class Seq2SeqRNNEncoder(nn.Module):
+    """
+    Seq2seq rnn encoder.
+    """
+
     def __init__(
         self,
         encoder_num_embeddings: int,
@@ -31,3 +35,82 @@ class Seq2SeqRNNEncoder(nn.Module):
         enc_emb = self.encoder_embedding(x)
         _, enc_last = self.encoder(enc_emb)
         return enc_last
+
+
+class Seq2SeqRNNDecoder(nn.Module):
+    """
+    Seq2seq rnn decoder.
+    """
+
+    def __init__(
+        self,
+        decoder_num_embeddings: int,
+        decoder_embedding_dim: int,
+        decoder_hidden_size: int,
+        decoder_num_layers: int,
+        decoder_dropout: Union[int, float],
+    ):
+        super().__init__()
+        self.decoder_embedding = nn.Embedding(
+            num_embeddings=decoder_num_embeddings,
+            embedding_dim=decoder_embedding_dim,
+            padding_idx=3,
+        )
+        self.decoder = nn.GRU(
+            input_size=decoder_embedding_dim,
+            hidden_size=decoder_hidden_size,
+            num_layers=decoder_num_layers,
+            dropout=decoder_dropout,
+            bidirectional=False,
+            batch_first=True,
+        )
+        self.linear = nn.Linear(
+            in_features=decoder_hidden_size,
+            out_features=decoder_num_embeddings,
+        )
+
+    def forward(self, x, enc_last):
+        dec_emb = self.decoder_embedding(x)
+        dec_out, _ = self.decoder(dec_emb, enc_last)
+        logits = self.linear(dec_out)
+        return logits
+
+
+class Seq2SeqRNN(nn.Module):
+    """
+    Seq2seq rnn model.
+    """
+
+    def __init__(
+        self,
+        encoder_num_embeddings: int,
+        encoder_embedding_dim: int,
+        encoder_hidden_size: int,
+        encoder_num_layers: int,
+        encoder_dropout: Union[int, float],
+        decoder_num_embeddings: int,
+        decoder_embedding_dim: int,
+        decoder_hidden_size: int,
+        decoder_num_layers: int,
+        decoder_dropout: Union[int, float],
+    ):
+        super().__init__()
+        self.encoder = Seq2SeqRNNEncoder(
+            encoder_num_embeddings=encoder_num_embeddings,
+            encoder_embedding_dim=encoder_embedding_dim,
+            encoder_hidden_size=encoder_hidden_size,
+            encoder_num_layers=encoder_num_layers,
+            encoder_dropout=encoder_dropout,
+        )
+        self.decoder = Seq2SeqRNNDecoder(
+            decoder_num_embeddings=decoder_num_embeddings,
+            decoder_embedding_dim=decoder_embedding_dim,
+            decoder_hidden_size=decoder_hidden_size,
+            decoder_num_layers=decoder_num_layers,
+            decoder_dropout=decoder_dropout,
+        )
+
+    def forward(self, from_seq, to_seq):
+        enc_last = self.encoder(from_seq)
+        logits = self.decoder(to_seq, enc_last)
+        return logits
