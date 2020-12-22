b0ea88ba643b46948ee3d4067879a4f06349293d
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-22 23:56:15 +0300

    update metrics

--- metrics.py
+++ metrics.py
@@ -1,26 +1,29 @@
 from typing import DefaultDict, List
+from warnings import filterwarnings
 
-from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
+
+filterwarnings(action="ignore", category=UserWarning)
 
 
 def calculate_metrics(
     metrics: DefaultDict[str, List[float]],
     loss: float,
-    y_true: List[int],
-    y_pred: List[int],
+    y_true: List[List[int]],
+    y_pred: List[List[int]],
 ) -> DefaultDict[str, List[float]]:
     """
     Calculate metrics on epoch.
     """
 
     smoothing_function = SmoothingFunction()
-    bleu_score = sentence_bleu(
-        references=[y_true],
-        hypothesis=[y_pred],
-        smoothing_function=smoothing_function.method6,
+    bleu_score = corpus_bleu(
+        list_of_references=[[seq] for seq in y_true],
+        hypotheses=y_pred,
+        smoothing_function=smoothing_function.method0,  # no smoothing
     )
 
     metrics["loss"].append(loss)
-    metrics["bleu_score"].extend(bleu_score)
+    metrics["bleu_score"].append(bleu_score)
 
     return metrics
