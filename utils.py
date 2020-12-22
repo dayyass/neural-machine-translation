ded728c2d136bae1cb9eb3509bbd6a1df5b6637e
Dani El-Ayyass <dayyass@yandex.ru>
2020-12-22 16:52:03 +0300

    update utils

--- utils.py
+++ utils.py
@@ -15,3 +15,13 @@ def set_global_seed(seed: int):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
+
+
+def to_numpy(tensor: torch.Tensor) -> np.ndarray:
+    """
+    Convert torch.Tensor to np.ndarray.
+    """
+
+    return (
+        tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+    )
